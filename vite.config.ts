ac37dd4b8a4ab0cd6f61952142e2cfb8bdfd0ccd
SFM <octavio.her1707@gmail.com>
2026-03-23 00:19:37 -0600

    SIXW

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-
+base: "/Las_Cazuelas_Catering/",
   server: {
     host: "::",
     port: 8080,
