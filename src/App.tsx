7e5e6a9083ef6774a5768b292a8a7921c7c13529
SFM <octavio.her1707@gmail.com>
2026-03-22 23:14:29 -0600

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Las_Cazuelas_Catering">
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
